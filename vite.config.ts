13269cfdc75b33af14a1ba63c0fff78eec5e8f18
MotoyaAsahina <motoyaasahina@icloud.com>
2023-02-16 15:08:50 +0900

    Update proxy setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
-import { setDefaultResultOrder } from 'dns'
 
-setDefaultResultOrder('ipv4first')
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '/@': path.resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8010'
+  // https://vitejs.dev/config/
+  return defineConfig({
+    resolve: {
+      alias: {
+        '/@': path.resolve(__dirname, 'src')
       }
-    }
-  },
-  plugins: [vue(), WindiCSS()]
-})
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_ADDR,
+          changeOrigin: true,
+          headers: {
+            cookie: process.env.VITE_API_COOKIE
+          }
+        }
+      }
+    },
+    plugins: [vue(), WindiCSS()]
+  })
+}
