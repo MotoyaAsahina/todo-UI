963a1d2f08f912a1f5b0dc976b3621618e5abde9
MotoyaAsahina <motoyaasahina@icloud.com>
2023-02-17 00:12:43 +0900

    Show task editor

---
+++ src/components/TaskEditor/TaskEditor.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import CheckIcon from '/@/components/UI/Icon/CheckIcon.vue'
+import CloseIcon from '/@/components/UI/Icon/CloseIcon.vue'
+
+const group = defineProps<{ id: string }>()
+</script>
+
+<template>
+  <div
+    class="w-18rem p-2.4 rounded-lg border-1 border-gray-200 bg-white shadow-md"
+  >
+    <div class="mb-2">
+      <h3 class="text-base font-semibold">[[ newOrEdit() ]] Task</h3>
+    </div>
+
+    <textarea
+      :id="`task-editor-input-${group.id}`"
+      v-model="rawTaskData"
+      class="w-full p-1 text-sm resize-y border-1 border-gray-400"
+      rows="6"
+    ></textarea>
+
+    <div class="flex items-center justify-end mt-2">
+      <a @click="closeEditors"><close-icon /></a>
+      <a @click="postTask"><check-icon class="ml-0.5" /></a>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/TaskPanel/TaskPanel.vue
+++ src/components/TaskPanel/TaskPanel.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import TaskPanelHeader from '/@/components/TaskPanel/TaskPanelHeader.vue'
+import TaskEditor from '/@/components/TaskEditor/TaskEditor.vue'
 import TaskList from '/@/components/TaskPanel/TaskList.vue'
+import TaskPanelHeader from '/@/components/TaskPanel/TaskPanelHeader.vue'
 import { tasks } from '/@/lib/fetch'
 
 const group = defineProps<{
@@ -11,12 +12,17 @@ const group = defineProps<{
 
 <template>
   <div class="w-84 p-1.6 h-full <sm:(snap-start snap-m-1.6)">
-    <div class="w-80.8 h-full rounded-lg border-1 border-gray-200 bg-gray-50">
+    <div
+      class="w-80.8 h-full rounded-lg border-1 border-gray-200 bg-gray-50 relative"
+    >
       <task-panel-header
         :group-id="group.id"
         :length="tasks[group.id]?.length || 0"
         :name="group.name"
       />
+
+      <task-editor :id="group.id" class="right-1 top-9 absolute z-8" />
+
       <task-list :group-id="group.id" />
     </div>
   </div>
